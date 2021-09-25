a2f39f6b27e6ae301dd2cf08008d5601cd28cfc7
Michael Joshua Smith <michaeljoshuasmith1@Michaels-MacBook-Air.local>
2021-09-24 21:22:45 -0400

    Attempting to solve challenge #3

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # read csv file from filesystem
 df = pd.read_csv('mental-health-in-tech-survey-2019.csv')
 
-
+# Protected Class Variable 1: Race
+dfCaucasian
+dfBlack
+dfHispanic
+dfAsian
+dfMixed
 
 # traverse through all records of survey dataframe
-# for index, record in df.iterrows():
-#     print(type(record))
-#     exit()
+#for index, record in df.iterrows():
+#    print(record)
+#    if "what" in record:
+#        print(record)
 
 
 # total number of columns
